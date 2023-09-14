9b39ed10d0f8ed0403905c5b6dae067b792a5151
Charles Lee <unknownsanon@gmail.com>
2023-09-14 11:10:47 +0800

    Keep track score

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
 require 'sinatra'
 enable :sessions
 
-get '/' do 
-
-erb :index
+get '/' do
+    erb :index
 end
 
 post '/start' do
@@ -14,21 +13,27 @@ end
 post '/result' do
     @player_choice = params[:choice]
     @computer_choice = ["rock", "paper", "scissors"].sample
+    session[:wins] ||= 0
+    session[:losses] ||= 0
+    session[:draw] ||= 0
 
     case
     when (@player_choice == 'rock' && @computer_choice == 'scissors') ||
         (@player_choice == 'scissors' && @computer_choice == 'paper') ||
         (@player_choice == 'paper' && @computer_choice == 'rock')
-    @outcome = "You won!"
+    session[:wins] += 1
+    @outcome = "You won! | AI lose!"
     when (@player_choice == 'scissors' && @computer_choice == 'rock') ||
         (@player_choice == 'paper' && @computer_choice == 'scissors') ||
         (@player_choice == 'rock' && @computer_choice == 'paper')
-    @outcome = "You lose!"
+    session[:losses] += 1
+    @outcome = "You lose! You lose!"
     else
     @outcome = "Draw!"
+    session[:draw] += 1
     end
 
     puts @outcome
-  
-  erb :result
+
+    erb :result
   end
